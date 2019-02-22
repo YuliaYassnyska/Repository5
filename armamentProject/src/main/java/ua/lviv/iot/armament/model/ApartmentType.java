b1e0ddc3a9db7d94478f0cd90a90078f763bf4d5
Yulia Yasnyska <yuliayasso@gmail.com>
2019-02-21 23:22:35 -0800

    Lab4

---
+++ armamentProject/src/main/java/ua/lviv/iot/armament/model/ApartmentType.java
@@ -0,0 +1,6 @@
+package ua.lviv.iot.armament.model;
+
+public enum ApartmentType {
+
+   PISTOLS, GRENADES, SNIPERDEVISE;
+}
---
+++ armamentProject/src/main/java/ua/lviv/iot/armament/model/Pistol.java
@@ -0,0 +1,40 @@
+package ua.lviv.iot.armament.model;
+
+public class Pistol extends Armament {
+
+    private int caliber;
+    private String origin;
+
+    public Pistol() {
+    }
+
+    public Pistol(final ApartmentType type,
+            final double price,
+            final Power power,
+            final Use use,
+            final User user,
+            final int amount,
+            final int caliber,
+            final String origin) {
+        super(type, price, power, use, user, amount);
+        this.caliber = caliber;
+        this.origin = origin;
+        }
+
+    public final int getCaliber() {
+         return caliber;
+    }
+
+    public final void setCaliber(final int caliber) {
+        this.caliber = caliber;
+    }
+
+    public final String getOrigin() {
+        return origin;
+    }
+
+    public final void setOrigin(final String origin) {
+        this.origin = origin;
+    }
+
+}
